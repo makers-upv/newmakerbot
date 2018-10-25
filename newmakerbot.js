a1634924f015ff14913874c26a961e22127acd4b
Carlos Precioso <kalprestito@gmail.com>
2018-10-25 15:18:46 +0200

    Usar `const` en lugar de `var`

--- newmakerbot.js
+++ newmakerbot.js
@@ -8,37 +8,32 @@ Creación y matenimiento: Jaime Laborda - jaimelaborda@gmail.com
 ----------------------------------------------------------------------------*/
 
 const Telegraf = require('telegraf');
-const Token = 'BotFather_token_goes_here';
-
-const ranapepe = ['CAADBAADOQ4AAjZHEwABYUGtfJvIDiAC', 'CAADBAADbAYAAjZHEwABe0Wm_QNyWgcC', 'CAADBAADXgYAAjZHEwAB-2vZhZcVhRMC', 'CAADBAADhAYAAjZHEwABY_4JnOLbxX0C', 'CAADBAADhgYAAjZHEwABp_fhAiD4_GEC', 'CAADBAADdgYAAjZHEwABx9tJw309ZgABAg', 'CAADBAADXAYAAjZHEwABabKzEznrA-wC', 'CAADBAADWAYAAjZHEwABHT5IHCj5Tt4C', 'CAADBAADxw8AAjZHEwAB2twhZ_iM-jwC', 'CAADBAADfgYAAjZHEwABqUjJxUJ9ifgC', 'CAADBAADgAYAAjZHEwABbgj3xbMopMcC'];
-
-const bot = new Telegraf(Token);
+const token = 'BotFather_token_goes_here';
+const bot = new Telegraf(token);
 
 bot.command('testbot', (ctx) => {
-  var nombre = ctx.from.first_name;
-
+  const nombre = ctx.from.first_name;
   ctx.reply("Hola " + nombre + ", estoy vivo!");
 });
 
 
+const ranapepe = ['CAADBAADOQ4AAjZHEwABYUGtfJvIDiAC', 'CAADBAADbAYAAjZHEwABe0Wm_QNyWgcC', 'CAADBAADXgYAAjZHEwAB-2vZhZcVhRMC', 'CAADBAADhAYAAjZHEwABY_4JnOLbxX0C', 'CAADBAADhgYAAjZHEwABp_fhAiD4_GEC', 'CAADBAADdgYAAjZHEwABx9tJw309ZgABAg', 'CAADBAADXAYAAjZHEwABabKzEznrA-wC', 'CAADBAADWAYAAjZHEwABHT5IHCj5Tt4C', 'CAADBAADxw8AAjZHEwAB2twhZ_iM-jwC', 'CAADBAADfgYAAjZHEwABqUjJxUJ9ifgC', 'CAADBAADgAYAAjZHEwABbgj3xbMopMcC'];
+
 bot.command('ranapepe', (ctx) => {
-  var chatId = ctx.chat.id;
-  var random = Math.floor(Math.random() * ranapepe.length); //Del 0 al 10
-
+  const chatId = ctx.chat.id;
+  const random = Math.floor(Math.random() * ranapepe.length); //Del 0 al 10
   console.log("Random: " + random);
-
   ctx.telegram.sendSticker(chatId, ranapepe[random]);
-
 });
 
 
 //Cuando un nuevo usuario
 bot.on('new_chat_members', (ctx) => {
   console.log(ctx.message.new_chat_participant);
-  var nombre = ctx.message.new_chat_participant.first_name;
-  var apellido = ctx.message.new_chat_participant.last_name;
-  var username = ctx.message.new_chat_participant.username;
-  var isBot = ctx.message.new_chat_participant.is_bot;
+  const nombre = ctx.message.new_chat_participant.first_name;
+  const apellido = ctx.message.new_chat_participant.last_name;
+  const username = ctx.message.new_chat_participant.username;
+  const isBot = ctx.message.new_chat_participant.is_bot;
 
   if (apellido == null) {
     apellido = " ";
